b438e0a71944d2938af64fd9463551b09849b1b3
hanilizalo23 <nelida_phm@hotmail.com>
2022-05-02 01:21:24 -0500

    Files for hardware buttons

---
+++ Switches.h
@@ -0,0 +1,60 @@
+/**
+ * \file		Switches.h
+ * \brief
+ * 		Source file for the Switches (SW2 and SW3) device driver for Kinetis K64.
+ * 		This is the application programming interface (API) for the Switches.
+ * 		It contains the definition of the special variables and functions
+ * 		for the configuration of the Switches.
+ *  \date:		01/05/2022
+ *  \author:	Nelida Hernández
+ */
+
+#ifndef SWITCHES_H_
+#define SWITCHES_H_
+
+
+#define SW2_bit bit_6
+#define SW3_bit bit_4
+
+typedef enum{SW2,
+			 SW3
+			} sw_name_t;
+
+/*
+ 	 \brief
+ 	 	 This function configures the GPIO needed for the SW2 with its pull-up resistor enabled.
+ 	 \param[in] void.
+ 	 \return void.
+*/
+uint8_t SW2_config(void);
+/*
+ 	 \brief
+ 	 	 This function disables SW2.
+ 	 \param[in] void.
+ 	 \return void.
+*/
+uint8_t SW2_disable(void);
+/*
+ 	 \brief
+ 	 	 This function configures the GPIO needed for the SW3 with its pull-up resistor enabled.
+ 	 \param[in] void.
+ 	 \return void.
+*/
+uint8_t SW3_config(void);
+/*
+ 	 \brief
+ 	 	 This function disables SW3.
+ 	 \param[in] void.
+ 	 \return void.
+*/
+uint8_t SW3_disable(void);
+/*
+ 	 \brief
+ 	 	 This function reads the state (input) from the corresponding switch.
+ 	 \param[in] sw The number of the switch to read.
+ 	 \return 1 if the switch is not pressed, 0 if it is pressed.
+*/
+uint8_t sw_state(sw_name_t sw);
+
+
+#endif /* SWITCHES_H_ */
